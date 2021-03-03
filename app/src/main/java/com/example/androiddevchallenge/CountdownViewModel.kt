e705e3be8d8f5c467509a6d6b60d0e3d21b26bbe
josh9595 <josh.everett@bbc.co.uk>
2021-03-03 21:03:30 +0000

    Create countdown timer

---
+++ app/src/main/java/com/example/androiddevchallenge/CountdownViewModel.kt
@@ -0,0 +1,128 @@
+package com.example.androiddevchallenge
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CountdownViewModel : ViewModel() {
+
+    private val _countdownState = MutableLiveData(CountdownState.IDLE)
+    val countdownState: LiveData<CountdownState> = _countdownState
+
+    private val _timerMinute = MutableLiveData(0)
+    val timerMinute: LiveData<Int> = _timerMinute
+
+    private val _timerOneSecond = MutableLiveData(0)
+    val timerOneSecond: LiveData<Int> = _timerOneSecond
+
+    private val _timerTwoSecond = MutableLiveData(0)
+    val timerTwoSecond: LiveData<Int> = _timerTwoSecond
+
+    fun minuteUp() {
+        _timerMinute.value?.let {
+            if (it == 9) {
+                _timerMinute.value = 0
+            } else {
+                _timerMinute.value = it + 1
+            }
+        }
+    }
+
+    fun minuteDown() {
+        _timerMinute.value?.let {
+            if (it == 0) {
+                _timerMinute.value = 9
+            } else {
+                _timerMinute.value = it - 1
+            }
+        }
+    }
+
+    fun oneSecondUp() {
+        _timerOneSecond.value?.let {
+            if (it == 5) {
+                _timerOneSecond.value = 0
+            } else {
+                _timerOneSecond.value = it + 1
+            }
+        }
+    }
+
+    fun oneSecondDown() {
+        _timerOneSecond.value?.let {
+            if (it == 0) {
+                _timerOneSecond.value = 5
+            } else {
+                _timerOneSecond.value = it - 1
+            }
+        }
+    }
+
+    fun twoSecondUp() {
+        _timerTwoSecond.value?.let {
+            if (it == 9) {
+                _timerTwoSecond.value = 0
+            } else {
+                _timerTwoSecond.value = it + 1
+            }
+        }
+    }
+
+    fun twoSecondDown() {
+        _timerTwoSecond.value?.let {
+            if (it == 0) {
+                _timerTwoSecond.value = 9
+            } else {
+                _timerTwoSecond.value = it - 1
+            }
+        }
+    }
+
+    fun secondDown() {
+        if (_timerTwoSecond.value != 0) {
+            _timerTwoSecond.value?.let { _timerTwoSecond.value = it - 1 }
+            if (
+                _timerTwoSecond.value == 0 &&
+                _timerOneSecond.value == 0 &&
+                _timerMinute.value == 0
+            ) {
+                _countdownState.value = CountdownState.IDLE
+            }
+        } else {
+            _timerTwoSecond.value?.let { _timerTwoSecond.value = 9 }
+            if (_timerOneSecond.value != 0) {
+                _timerOneSecond.value?.let { _timerOneSecond.value = it - 1 }
+            } else {
+                _timerOneSecond.value?.let { _timerOneSecond.value = 5 }
+                if (_timerMinute.value != 0) {
+                    _timerMinute.value?.let { _timerMinute.value = it - 1 }
+                } else {
+                    _countdownState.value = CountdownState.IDLE
+                }
+            }
+        }
+    }
+
+    fun startTimer() {
+        _countdownState.value = CountdownState.COUNTING
+    }
+
+    fun pauseTimer() {
+        _countdownState.value = CountdownState.PAUSED
+    }
+
+    fun stopTimer() {
+        _countdownState.value = CountdownState.IDLE
+        _timerMinute.value?.let { _timerMinute.value = 0 }
+        _timerOneSecond.value?.let { _timerOneSecond.value = 0 }
+        _timerTwoSecond.value?.let { _timerTwoSecond.value = 0 }
+    }
+
+}
+
+enum class CountdownState {
+    IDLE,
+    PAUSED,
+    COUNTING,
+}
--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -16,16 +16,26 @@
 package com.example.androiddevchallenge
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -38,10 +48,132 @@ class MainActivity : AppCompatActivity() {
 
 // Start building your app here!
 @Composable
-fun MyApp() {
+fun MyApp(countdownViewModel: CountdownViewModel = CountdownViewModel()) {
+    val countdownState: CountdownState by countdownViewModel.countdownState.observeAsState(CountdownState.IDLE)
+    val timerMinute: Int by countdownViewModel.timerMinute.observeAsState(0)
+    val timerOneSecond: Int by countdownViewModel.timerOneSecond.observeAsState(0)
+    val timerTwoSecond: Int by countdownViewModel.timerTwoSecond.observeAsState(0)
+    val mainHandler = Handler(Looper.getMainLooper())
+
+    mainHandler.post(object : Runnable {
+        override fun run() {
+            if (countdownState == CountdownState.COUNTING) countdownViewModel.secondDown()
+            mainHandler.postDelayed(this, 1000)
+        }
+    })
+
     Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+        Column(modifier = Modifier.padding(16.dp)) {
+            Row(modifier = Modifier.align(Alignment.CenterHorizontally).fillMaxWidth()) {
+                Column() {
+                    Button(onClick = { countdownViewModel.minuteUp() }) {
+                        Text(
+                            text = "▲",
+                            style = MaterialTheme.typography.h4
+                        )
+                    }
+                    Text(
+                        text = "$timerMinute",
+                        modifier = Modifier.padding(start = 8.dp, end = 8.dp),
+                        style = MaterialTheme.typography.h1
+                    )
+                    Button(onClick = { countdownViewModel.minuteDown() }) {
+                        Text(
+                            text = "▼",
+                            style = MaterialTheme.typography.h4
+                        )
+                    }
+                }
+
+                Column() {
+                    Text(
+                        text = ":",
+                        modifier = Modifier.padding(top = 56.dp),
+                        style = MaterialTheme.typography.h1
+                    )
+                }
+
+                Column() {
+                    Button(onClick = { countdownViewModel.oneSecondUp() }) {
+                        Text(
+                            text = "▲",
+                            style = MaterialTheme.typography.h4
+                        )
+                    }
+                    Text(
+                        text = "$timerOneSecond",
+                        modifier = Modifier.padding(start = 8.dp, end = 8.dp),
+                        style = MaterialTheme.typography.h1
+                    )
+                    Button(onClick = { countdownViewModel.oneSecondDown() }) {
+                        Text(
+                            text = "▼",
+                            style = MaterialTheme.typography.h4
+                        )
+                    }
+                }
+
+                Column() {
+                    Button(onClick = { countdownViewModel.twoSecondUp() }) {
+                        Text(
+                            text = "▲",
+                            style = MaterialTheme.typography.h4
+                        )
+                    }
+                    Text(
+                        text = "$timerTwoSecond",
+                        modifier = Modifier.padding(start = 8.dp, end = 8.dp),
+                        style = MaterialTheme.typography.h1
+                    )
+                    Button(onClick = { countdownViewModel.twoSecondDown() }) {
+                        Text(
+                            text = "▼",
+                            style = MaterialTheme.typography.h4
+                        )
+                    }
+                }
+            }
+
+            Text(
+                text = "State - $countdownState",
+                modifier = Modifier.padding(bottom = 8.dp),
+                style = MaterialTheme.typography.h5
+            )
+
+            Row() {
+                Button(
+                    onClick = { countdownViewModel.startTimer() },
+                    enabled = isStartButtonEnabled(countdownState)
+                ) {
+                    Text(text = "Start")
+                }
+                Button(
+                    onClick = { countdownViewModel.pauseTimer() },
+                    enabled = isPauseButtonEnabled(countdownState)
+                ) {
+                    Text(text = "Pause")
+                }
+                Button(
+                    onClick = { countdownViewModel.stopTimer() },
+                    enabled = isStopButtonEnabled(countdownState)
+                ) {
+                    Text(text = "Stop")
+                }
+            }
+        }
     }
+}
+
+fun isStartButtonEnabled(countdownState: CountdownState): Boolean {
+    return countdownState != CountdownState.COUNTING
+}
+
+fun isPauseButtonEnabled(countdownState: CountdownState): Boolean {
+    return countdownState == CountdownState.COUNTING
+}
+
+fun isStopButtonEnabled(countdownState: CountdownState): Boolean {
+    return countdownState != CountdownState.IDLE
 }
 
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
